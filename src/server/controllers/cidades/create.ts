1cc3b7e1f83ad77d632b9e477d54f4423a4b7bd9
Luanderson Souza <jlgs@ic.ufal.br>
2026-02-19 23:38:16 -0300

    refactor (create)
    Ajustes no fluxo de validação;
    Criada a função 'createValidation'.

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,46 +1,33 @@
-import { Request, RequestHandler, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express';
 import * as yup from 'yup';
+import { validation } from '../../shared/middlewares';
 
 interface Cidade {
     nome: string;
     estado: string;
 }
 
-const bodyValidation: yup.Schema <Cidade> = yup.object().shape({
-
-    nome: yup.string().required().min(3),
-    estado: yup.string().required().min(2).max(2),
-
-});
-
+interface Filter{
+    filter?: string | undefined;
+}
 // Isso aqui é um middleware, ele é chamado antes do create e validará o corpo da
 //  requisição antes de "liberar" o create para ser executado.
 
-export const createBodyValidator: RequestHandler = async ( req, res, next) => {
-    try{
-        await bodyValidation.validate(req.body, {abortEarly: false});
-        return next();
-    }
-    catch (err) {
-
-        const yupError = err as yup.ValidationError;
-        const errors: Record<string, string> = {};
-
-        yupError.inner.forEach(error => {
-            if (error.path === undefined) return;
-            errors[error.path] = error.message;
-        });
-        
-        return res.status(StatusCodes.BAD_REQUEST).json({errors});
-    }
-};
+export const createValidation = validation((getSchema) => ({
+    body: getSchema<Cidade>(yup.object().shape({
+        nome: yup.string().required().min(3),
+        estado: yup.string().required().min(2).max(2),
+    })),
+    query: getSchema<Filter>(yup.object().shape({
+        filter: yup.string().optional().min(3),
+    })),
+}));
 
 // Já aqui é o create em si, ele só será executado se o createBodyValidator obtiver sucesso no seu 'try'
 //  e chamar o 'next()'; Ou seja, no meu arquivo de rotas, na minha rota '/cidades', terei o createBodyValidator
 //  como um middleware, e então o create como handler da rota.
 
-export const create: RequestHandler = async (req: Request<{}, {}, Cidade>, res: Response) => {
+export const create = async (req: Request<{}, {}, Cidade>, res: Response) => {
 
   console.log(req.body);
 
