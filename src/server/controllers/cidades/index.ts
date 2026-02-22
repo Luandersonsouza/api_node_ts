72ca715466efb23c003f9612e8b3a7e43db2df8c
Luanderson Souza <jlgs@ic.ufal.br>
2026-02-22 00:06:14 -0300

    update ( index )
    Adição de métodos CRUD para crontroller de cidades.

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,7 +1,15 @@
 import * as create  from './create';
+import * as getAll from './getAll';
+import * as getByid from './getByid';
+import * as updateByid from './updateByid';
+import * as deleteByid from './deleteByid';
 
 export const cidadesController = {
 
     ...create,
+    ...getAll,
+    ...getByid,
+    ...deleteByid,
+    ...updateByid,
 };
 
