74fa6895d1994935effa60275e4f0c0dd3d7d28f
Luanderson Souza <jlgs@ic.ufal.br>
2026-02-22 00:03:30 -0300

    Update (create)
    Create atualizado, agora utilizando o yup para criação dos schemas e não mais implementando manual, prevenindo assim problemas de tipagem entre yup e Typescript.

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,35 +1,29 @@
 import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { validation } from '../../shared/middlewares';
+import { StatusCodes } from 'http-status-codes';
 
-interface Cidade {
-    nome: string;
-    estado: string;
-}
 
-interface Filter{
-    filter?: string | undefined;
-}
+const bodySchema = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
+
+type BodyProps = yup.InferType<typeof bodySchema>;
+
 // Isso aqui é um middleware, ele é chamado antes do create e validará o corpo da
 //  requisição antes de "liberar" o create para ser executado.
 
 export const createValidation = validation((getSchema) => ({
-    body: getSchema<Cidade>(yup.object().shape({
-        nome: yup.string().required().min(3),
-        estado: yup.string().required().min(2).max(2),
-    })),
-    query: getSchema<Filter>(yup.object().shape({
-        filter: yup.string().optional().min(3),
-    })),
+    body: getSchema<BodyProps>(bodySchema),
 }));
 
 // Já aqui é o create em si, ele só será executado se o createBodyValidator obtiver sucesso no seu 'try'
 //  e chamar o 'next()'; Ou seja, no meu arquivo de rotas, na minha rota '/cidades', terei o createBodyValidator
 //  como um middleware, e então o create como handler da rota.
 
-export const create = async (req: Request<{}, {}, Cidade>, res: Response) => {
+export const create = async (req: Request<{}, {}, BodyProps>, res: Response) => {
 
   console.log(req.body);
 
-  return res.send('Criado!');
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado');
 };
