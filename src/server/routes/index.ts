8fc4a175104f41bdc6e643dc78dbd84bd59bdc71
Luanderson Souza <jlgs@ic.ufal.br>
2026-02-22 00:07:39 -0300

    update ( index )
    Criação de novas rotas para métodos CRUD.

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,12 +3,33 @@ import {Router} from "express";
 import {cidadesController} from './../controllers';
 
 const router = Router();
+
 router.get('/', (_, res) => {
   return res.send({ message: 'Funcionando' });
 });
 
+//Criação das rotas de cidades
 router.post('/cidades',
    cidadesController.createValidation,
    cidadesController.create);
 
+//Pega sem filtro cidades
+router.get('/cidades',
+   cidadesController.getAllValidation,
+   cidadesController.getAll);
+
+//Pega cidades com filtro de ID
+router.get('/cidades/:id',
+   cidadesController.getByIdValidation,
+   cidadesController.getByid);
+
+//Atualiza cidades com filtro de ID   
+router.put('/cidades/:id',
+   cidadesController.updateByIdValidation,
+   cidadesController.updateByid);
+
+//Deleta cidades com filtro de ID   
+router.delete('/cidades/:id',
+   cidadesController.deleteByIdValidation,
+   cidadesController.deleteByid);
 export { router };
