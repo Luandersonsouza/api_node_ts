af797295a7d5b535a59f0cab83d7c71dcf25dc9a
Luanderson Souza <jlgs@ic.ufal.br>
2026-03-02 22:59:24 -0300

    Update create.ts
    Remoção do famoso console.log ('Passou Aqui')

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -23,7 +23,5 @@ export const createValidation = validation((getSchema) => ({
 
 export const create = async (req: Request<{}, {}, BodyProps>, res: Response) => {
 
-  console.log(req.body);
-
   return res.status(StatusCodes.CREATED).json(1);
 };
